e131b84de7cc3dd63797c44e28325796a040e2b3
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-05-14 11:24:03 +0200

    routes set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'user', component: UserProfileComponent},
+    {path: 'signup', component: SignUpComponent},
+    {path: '', component: UserProfileComponent}
+];
